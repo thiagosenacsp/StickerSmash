4e6b1ba0820f933cf73011d4d4a907b9a2ecdef8
Thiago Nobrega <thiago.fnobrega@senacsp.edu.br>
2023-11-23 22:02:10 -0300

    chore: update button props to include theme

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,32 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Pressable, StyleSheet, Text, View } from "react-native";
 
-export default function Button({ label }) {
+export default function Button({ label, theme }) {
+  if (theme === "primary") {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+          { borderWidth: 4, borderColor: "#FFD33D", borderRadius: 18 },
+        ]}
+      >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#FFF" }]}
+          onPress={() => alert("You pressed a button")}
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292E"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292E" }]}>
+            {label}
+          </Text>
+        </Pressable>
+      </View>
+    );
+  }
   return (
     <View style={styles.buttonContainer}>
       <Pressable
